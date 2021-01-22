d420db792d5292f9e0d743385b70113b69d32eed
Allen Liu <allen6qi@gmail.com>
2021-01-22 10:18:02 +0800

    connected vue3 with vuex4

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a class="item active"> Home </a>
+    <div class="right menu">
+      <a class="ui item"> Logout </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style>
+</style>
