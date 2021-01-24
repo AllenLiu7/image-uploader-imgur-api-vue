bced99bf2a70b84cf14d314a02a22388869098bf
Allen Liu <allen6qi@gmail.com>
2021-01-24 21:46:10 +0800

    added login status persistan

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -3,17 +3,20 @@ import qs from 'qs'
 
 export default {
     state: () => ({
-        token: null
+        token: window.localStorage.getItem('imgur_token')
     }),
     getters : {
         isLoggedin: state => !!state.token
     },
     actions:{
         login: ()=>api.login(),
+        
         finalizeLogin: ({commit}, hash)=>{
             const query = qs.parse(hash.replace('#', ''));
             commit('setToken', query.access_token)
+            window.localStorage.setItem('imgur_token', query.access_token)
         },
+        
         logout: ({commit})=>{
             commit('setToken', null)
         }
